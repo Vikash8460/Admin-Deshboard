bdfec3860ca76550755ba398a718b65aff5f2823
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-06 13:31:02 +0530

    Make Home components

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
+          Here you'll find a variety of articles and tutorials on topics such as
+          web development, software engineering, and programming languages.
+        </p>
+        <Link
+          to='/'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          View all posts
+        </Link>
+      </div>
+
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+       
+      </div>
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+       
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4'>
+      
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        
+      </div>
+    </div>
   )
 }
